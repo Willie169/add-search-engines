981a19acbdd2691e7ff01cf99906b37f8bd6e32d
Willie Shen <cg10250207@gmail.com>
2026-08-13 23:21:32 +0800

    2026-08-13T15:21:32+00:00

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+rm -f Peskyfox.js user.js
 wget https://github.com/yokoffing/Betterfox/raw/refs/heads/main/Peskyfox.js
 wget https://github.com/arkenfox/user.js/raw/refs/heads/master/user.js
 cat Peskyfox.js arkenfox-overrides.js Peskyfox-overrides.js extra-overrides.js >user-overrides.js
